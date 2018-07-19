8c92c7c248915b9e940529730f0bef16438d7786
Michel Marques da Silva <m.marques.dev@gmail.com>
2018-07-19 00:20:48 -0300

    refatoração do codigo pra separar as funções e reutilizalas

--- create.js
+++ create.js
@@ -1,14 +1,9 @@
 import uuid from "uuid";
-import AWS from "aws-sdk";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
 
-AWS.config.update({ region: "us-east-1" });
-
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-export function main(event, context, callback) {
-    
+export async function main(event, context, callback) {
     const data = JSON.parse(event.body);
-
     const params = {
         TableName: "notes",
         Item: {
@@ -20,28 +15,10 @@ export function main(event, context, callback) {
         }
     };
 
-    dynamoDb.put(params, (error, data) => {
-        const headers = {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Credentials": true
-        };
-
-        if (error) {
-            const response = {
-                statusCode: 500,
-                headers: headers,
-                body: JSON.stringify({ status: false })
-            };
-
-            callback(null, response);
-            return;
-        }
-
-        const response = {
-            statusCode: 200,
-            headers: headers,
-            body: JSON.stringify(params.Item)
-        };
-        callback(null, response);        
-    });
+    try {
+        await dynamoDbLib.call("put", params);
+        callback(null, success(params.Item));
+    } catch (e) {
+        callback(null, failure({ status: false }));
+    }
 }
